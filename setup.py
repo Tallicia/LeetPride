5263a45b3207e3b3c78ed6f43cb8c66169937db6
Alicia Yingling <AliciaYingling@gmail.com>
2022-06-05 15:31:23 -0500

    v0.69.24

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LeetPride",
-    version="0.6.9.2.3",
+    version="0.6.9.2.4",
     author="Alicia Yingling",
     author_email="AliciaYingling@gmail.com",
     description="LeetPride makes for a beautiful problem solving and learning environment for test and timing data "
