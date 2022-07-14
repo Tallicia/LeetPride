3a62c88e3c50cad8925e5de1c9b1b8516e3caf77
Tallicia <AliciaYingling@gmail.com>
2022-07-13 19:17:45 -0500

    Updating for helper methods on TreeNode plus version rev/bump

--- LeetPride/__init__.py
+++ LeetPride/__init__.py
@@ -1,6 +1,7 @@
 # __init__.py
 from .Core import completion_display, run_process, str_style, eprint, timeit, LeetPrideCore
 from .ListNode import ListNode, list_nodes, make_node_list, make_node_lists, append_node_list
-from .TreeNode import TreeNode, list_tree, list_tree_pre, list_level_order, list_current_level, height
+from .TreeNode import TreeNode, make_tree, insert_tree_node, \
+    list_tree, list_tree_pre, list_level_order, list_current_level, height
 from .Examples import lpccm_example_main, lpcnl1_example_main, lpccp_example_main, \
     lpcm1_example_main, lpcmm_example_main
